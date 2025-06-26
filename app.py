1aaf00ef424a800370dd893d2a7d692c2038722a
Khaled Raslan <khaledraslan32@gmail.com>
2025-06-26 21:41:57 +0300

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ col1, col2 = st.columns([1, 1])
 with col1:
     st.image("OIP.jfif", width=150)
 with col2:
-    st.image("picocheiron_logo.jpg", width=150)
+    st.image("picocheiron_logo.jpeg", width=150)
 
 st.title("Gas Wells Production Rate Predictor")
 st.markdown("Upload a file or manually input well data to predict **Gas**, **Condensate**, and **Water** rates.")
